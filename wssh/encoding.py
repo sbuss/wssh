4797f4aadc1eaf25d8bbc8f1c670b9ee61be034e
Steven Buss <steven.buss@gmail.com>
2015-11-20 22:18:30 -0800

    Don't double-encode bytes and properly encode strings

--- wssh/encoding.py
+++ wssh/encoding.py
@@ -1,7 +1,10 @@
 import base64
+import six
 
 
 def encode(data):
+    if isinstance(data, six.string_types):
+        data = bytes(data, 'utf-8')
     return str(base64.b64encode(data), 'utf-8')
 
 
